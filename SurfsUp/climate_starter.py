74aadee861d4e140a46a650c82e63567a181585f
Jose Eneas da Silva Maria <joseeneas@me.com>
2023-06-23 13:36:42 -0400

    SQLALCHEMY CHALLENGE

--- SurfsUp/climate_starter.py
+++ SurfsUp/climate_starter.py
@@ -1,269 +1,191 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-# Preparing the basics... Import required modules
-
-#get_ipython().run_line_magic('matplotlib', 'inline')
-from   matplotlib             import style
-import matplotlib.pyplot      as     plt
+# Import the dependencies.
 import numpy                  as     np
 import sqlalchemy
 from   sqlalchemy.ext.automap import automap_base
 from   sqlalchemy.orm         import Session
 from   sqlalchemy             import create_engine, func
-from   sqlalchemy.inspection  import inspect
-from   collections            import defaultdict
+from   flask                  import Flask, jsonify, request
 import datetime               as     dt
-import pandas                 as     pd
 
-
-# In[3]:
-
-
-# Creating a auxiliary function to translate a query result set into a pandas dataframe
-
-def query_to_dict(rset):
-    result = defaultdict(list)
-    for obj in rset:
-        instance = inspect(obj)
-        for key, x in instance.attrs.items():
-            result[key].append(x.value)
-    return result
-
-
-# In[4]:
-
-
-# create engine to hawaii.sqlite
-
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
-
-# In[5]:
-
-
-# reflect an existing database into a new model
-
-Base = automap_base()
-Base.prepare(autoload_with=engine)
-Base.classes.keys()
-
+#################################################
+# Database Setup
+#################################################
+# create the engine for the sqlite database
+# reflect the database into a model
 # reflect the tables
+# save the references
+engine           = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base             = automap_base()
+Base.prepare(autoload_with=engine)
+MeasurementTable = Base.classes.measurement
+StationTable     = Base.classes.station
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    #
+    # 3.1
+    #
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"<br/>"
+        f"<strong>/ (the home page)</strong><br/>"
+        f"<br/>"
+        f"....This page<br/>"
+        f"....List all the available routes<br/>"
+        f"<br/>"
+        f"<strong>/api/v1.0/precipitation</strong><br/>"
+	    f"<br/>"
+        f"....Convert the query results from your precipitation analysis (i.e. retrieve</strong><br/>"
+	    f"....only the last 12 months of data) to a dictionary using date as the key</strong><br/>"
+	    f"....and prcp as the value.</strong><br/>"
+	    f"....Return the JSON representation of your dictionary.</strong><br/>"
+        f"<br/>"
+        f"<strong>/api/v1.0/stations</strong><br/>"
+        f"<br/>"
+        f"....Return a JSON list of stations from the dataset.<br/>"
+        f"<br/>"
+        f"<strong>/api/v1.0/tobs</strong><br/>"
+        f"<br/>"
+        f"....Query the dates and temperature observations of the most-active<br/>"
+	    f"....station for the previous year of data.<br/>"
+	    f"....Return a JSON list of temperature observations for the previous year<br/>"
+        f"<br/>"
+        f"<strong>/api/v1.0/period?begin_date=yyyy-mm-dd</strong><br/>"
+        f"<strong>/api/v1.0/period?end_date=yyyy-mm-dd</strong><br/><br/>"
+        f"<strong>or combinations of begin_date and end_date parameters</strong><br/><br/>"
+        f"....Return a JSON list of the minimum temperature, the average<br/>"
+	    f"....temperature, and the maximum temperature for a specified start or<br/>"
+	    f"....start-end range.<br/>"
+	    f"....For a specified start, calculate TMIN , TAVG , and TMAX for all the dates<br/>"
+	    f"....greater than or equal to the start date.<br/>"
+	    f"....For a specified start date and end date, calculate TMIN , TAVG , and TMAX for<br/>"
+	    f"....the dates from the start date to the end date, inclusive.<br/>"
+    )
 
-Tables = Base.classes.keys()
+@app.route("/api/v1.0/precipitation")
+def precipitationdata():
+    #
+    # 3.2
+    #
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    max_date = session.query(MeasurementTable.date).\
+                             order_by(MeasurementTable.date.desc()).first()
+    # Calculating date limits
+    High_date = dt.datetime.strptime(max_date.date, "%Y-%m-%d")
+    Low_date = High_date - dt.timedelta(days=366) 
+    # Retrieve the data and store it in a DataFrame
+    results    = session.query(MeasurementTable.date,\
+                               MeasurementTable.prcp).\
+                               filter(MeasurementTable.date <= High_date).\
+                               filter(MeasurementTable.date >= Low_date)
+    session.close()
+    # Convert list of tuples into normal list
+    all_precipitation = []
+    for date, prcp in results:
+        precipitation_dict         = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        all_precipitation.append(precipitation_dict)
+    return jsonify(all_precipitation)
 
+@app.route("/api/v1.0/stations")
+def stationsdata():
+    #
+    # 3.3
+    #
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    results = session.query(StationTable.station, \
+                            StationTable.name, \
+                            StationTable.latitude, 
+                            StationTable.longitude, \
+                            StationTable.elevation). \
+                            all()
+    session.close()
+    # Convert list of tuples into normal list
+    all_stations = []
+    for station, name, latitude, longitude, elevation in results:
+        station_dict = {}
+        station_dict["station"]   = station
+        station_dict["name"]      = name
+        station_dict["latitude"]  = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        all_stations.append(station_dict)
+    return jsonify({'stations':all_stations})
 
-# In[6]:
-
-
-# View all of the classes that automap found
-
-print(f"There are {len(Tables)} tables in this database")
-print(f"First is {Tables[0]}")
-print(f"Next  is {Tables[1]}")
-
-
-# In[7]:
-
-
-# Save references to each table
-measurement = Base.classes.measurement
-station     = Base.classes.station
-print(f"The reference for the first tab is {measurement}")
-print(f"The reference for the next  tab is {station}")
-
-
-# In[8]:
-
-
-# Create our session (link) from Python to the DB
-
-session = Session(engine)
-
-
-# In[9]:
-
-
-# Exploratory Precipitation Analysis
-#
-# 1.1 
-#
-# Find the most recent date in the data set.
-# Query retuns the highest value for date in the table measurement, as the order is descending
-
-max_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
-print(f"The most recent entry: {max_date.date}")
-
-
-# In[10]:
-
-
-#
-# 1.2 thru 1.6
-#
-# Design a query to retrieve the last 12 months of precipitation data and plot the results. 
-# Starting from the most recent data point in the database. 
-# Calculate the date one year from the last date in data set.
-# Perform a query to retrieve the data and precipitation scores
-# Save the query results as a Pandas DataFrame. Explicitly set the column names
-# Sort the dataframe by date
-# Use Pandas Plotting with Matplotlib to plot the data
-
-# Calculating date limits
-
-High_date = dt.datetime.strptime(max_date.date, "%Y-%m-%d")
-Low_date = High_date - dt.timedelta(days=366) 
-print(F"Query upper limit is {High_date}")
-print(F"Query lower limit is {Low_date}")
-
-# Retrieve the data and store it in a DataFrame
-
-Query12Month    = session.query(measurement).\
-                                filter(measurement.date <= High_date).\
-                                filter(measurement.date >= Low_date)
-Query12MonthDF = pd.DataFrame(query_to_dict(Query12Month))
-Query12MonthDF = Query12MonthDF.replace(np. nan,0)
-Query12MonthDF = Query12MonthDF.sort_values(by=['date'],ascending = True)
-Query12MonthDF = Query12MonthDF.rename(columns={"date": "Date", "prcp": "Precipitation"})
-del Query12MonthDF['id']
-del Query12MonthDF['station']
-del Query12MonthDF['tobs']
-
-# Work the Graph, labels, legend, etc.
-# Graphic option 1 - Matplotlib
-plt.figure(figsize=(20,20))
-plt.plot(Query12MonthDF.Date,Query12MonthDF.Precipitation,label='Precipitation')
-plt.legend()
-plt.grid()
-plt.xlabel('Dates',fontsize=12)
-plt.xticks(rotation = 90,fontsize=12)
-plt.xticks(np.arange(1, Query12MonthDF.Date.count(), 50))
-plt.ylabel('Inches')
-plt.title('Preciptation in inches/date')
-plt.locator_params(axis='x', nbins=50)
-plt.show()
-
-# Query12MonthDF.head()
-# Work the Graph, labels, legend, etc.
-# Graphic option 2 - Pandas
-
-Query12MonthDF.plot(figsize=(20,20),grid=True,x='Date',y='Precipitation',ylabel='inches', rot=90,title='Preciptation in inches/date')
-
-# Showing both techniques to get an appreciation of ease of use, complexity, etc.
-
-
-# In[11]:
-
-
-#
-# 1.7
-#
-# Use Pandas to calculate the summary statistics for the precipitation data
-
-print('Stat Summary for Precipitation')
-print('==============================')
-Q12Summ = pd.DataFrame(Query12MonthDF.Precipitation.describe())
-print(Q12Summ)
-
-
-# # Exploratory Station Analysis
-
-# In[12]:
-
-
-#
-# 2.1
-#
-# Design a query to calculate the total number of stations in the dataset
-Stations        = session.query(station).count()
-print(f"There are {Stations} Stations in the database")
-
-
-# In[13]:
-
-
-#
-# 2.2
-#  
-# Design a query to find the most active stations (i.e. which stations have the most rows?)
-# List the stations and their counts in descending order.
-countStation = session.query(measurement.station,\
-                             func.count(measurement.station).label("countstation")).\
-                             group_by(measurement.station).\
-                             order_by(func.count(measurement.station).desc())
-
-for line in countStation:
-    print(F"Station : {line.station} Instance count: {line.countstation}")
-
-
-# In[14]:
-
-
-# 
-# 2.3
-#
-# Using the most active station id from the previous query, calculate the lowest, highest, and average temperature.
-FirstStation = countStation.first().station
-print(F"The most frequent station : {FirstStation}")
-
-QueryStation  = session.query(measurement.station, \
-                              func.max(measurement.tobs).label("maxtemp"), \
-                              func.min(measurement.tobs).label("mintemp"),
-                              func.avg(measurement.tobs).label("avgtemp")). \
-                              filter(measurement.station == FirstStation)
-for line in QueryStation:
-    print(F"Station : {line.station} mintemp: {line.mintemp} maxtemp: {line.maxtemp} avgtemp : {line.avgtemp}")
-
-#print(QueryStation.statement)
-#print(QueryStation.maxtemp)
-#print(QueryStation.mintemp)
-
-
-# In[15]:
-
-
-# 
-# 2.4
-#
-# Using the most active station id
-# Query the last 12 months of temperature observation data for this station and plot the results as a histogram
-Query12MonthTemp = session.query(measurement.tobs.label("Temperature"), \
-                                 func.count(measurement.tobs).label("Frequency")). \
-                                 filter(measurement.date <= High_date).\
-                                 filter(measurement.date >= Low_date).\
-                                 filter(measurement.station == FirstStation).\
-                                 group_by(measurement.tobs)
-Temperature = []
-Frequency = []
-for line in Query12MonthTemp:
-    Temperature.append(line.Temperature)
-    Frequency.append(line.Frequency)
+@app.route("/api/v1.0/tobs")
+def tobsdata():
+    #
+    # 3.4
+    #
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    countStation = session.query(MeasurementTable.station,\
+                                 func.count(MeasurementTable.station).label("countstation")).\
+                                 group_by(MeasurementTable.station).\
+                                 order_by(func.count(MeasurementTable.station).desc())
+    FirstStation = countStation.first().station
+    results = session.query(MeasurementTable.station,\
+                            MeasurementTable.date,\
+                            MeasurementTable.prcp).\
+                            filter(MeasurementTable.station == FirstStation).all()
+    session.close()
+    # Convert list of tuples into normal list
+    all_tobs = []
+    for station, date, prcp in results:
+        tobs_dict            = {}
+        tobs_dict["station"] = station
+        tobs_dict["date"]    = date
+        tobs_dict["prcp"]    = prcp
+        all_tobs.append(tobs_dict)
     
-plt.figure(figsize=(20,20))    
-plt.bar(Temperature,Frequency,label='Frequency')
-plt.legend()
-plt.grid(axis = 'y')
-plt.xlabel('Temp')
-plt.ylabel('Freq')
-plt.title('Temp x Freq')
-plt.locator_params(axis='x', nbins=12)
-plt.show()
+    return jsonify(all_tobs)
 
+@app.route("/api/v1.0/period")
+def perioddata():
+    #
+    # 3.5, query with start and end dates to filter the query.
+    #
 
-# # Close Session
+    begin_date = request.args.get('begin_date')
+    end_date   = request.args.get('end_date')
+    print(begin_date)
+    print(end_date)
+    if end_date == None:
+        end_date='2099-12-31'
+    if begin_date == None:
+        begin_date='2000-01-01'
+    print(begin_date)
+    print(end_date)
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    results  = session.query(MeasurementTable.date,\
+                             func.max(MeasurementTable.tobs).label("maxtemp"),\
+                             func.min(MeasurementTable.tobs).label("mintemp"),\
+                             func.avg(MeasurementTable.tobs).label("avgtemp")).\
+                             group_by(MeasurementTable.date).filter(MeasurementTable.date <= end_date). \
+                                                             filter(MeasurementTable.date >= begin_date)
+    session.close()
+    # Convert list of tuples into normal list
+    all_data = []
+    for date, maxtemp, mintemp, avgtemp in results:
+        data_dict            = {}
+        data_dict["date"]    = date
+        data_dict["maxtemp"] = maxtemp
+        data_dict["mintemp"] = mintemp
+        data_dict["avgtemp"] = avgtemp
+        all_data.append(data_dict)
+    
+    return jsonify(all_data)
 
-# In[16]:
-
-
-# Close Session
-session.close()
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
